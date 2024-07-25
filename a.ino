1655e59c41d1944b4ad15ec6f3b241ba99aecfbe
S-abk <117982032+S-abk@users.noreply.github.com>
2024-07-26 00:28:18 +0100

    Update a.ino

--- a.ino
+++ a.ino
@@ -3,16 +3,25 @@
 void setup() {
   Serial.begin(9600);
   pinMode(PROXIMITY_PIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   int proximityState = digitalRead(PROXIMITY_PIN);
 
-  Serial.print("Proximity: ");
   if (proximityState == HIGH) {
-    Serial.println("Detected");
+    Serial.println("Proximity: Detected");
   } else {
-    Serial.println("Not Detected");
+    Serial.println("Proximity: Not Detected");
+  }
+
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    if (command == '1') {
+      digitalWrite(LED_BUILTIN, HIGH);
+    } else if (command == '0') {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
   }
 
   delay(1000); // Read every second

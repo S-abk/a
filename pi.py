32bf48546dcb94acafea346a3fc3ae961546e705
S-abk <117982032+S-abk@users.noreply.github.com>
2024-07-26 00:15:41 +0100

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,5 +1,8 @@
+from flask import Flask, request, render_template, redirect, url_for
 import serial
 import time
+
+app = Flask(__name__)
 
 arduino = serial.Serial('/dev/ttyACM0', 9600)
 
@@ -15,3 +18,17 @@ def read_sensor_data():
         data = arduino.readline().decode('utf-8').rstrip()
         return data
     return None
+
+@app.route('/')
+def index():
+    sensor_data = read_sensor_data()
+    return render_template('index.html', sensor_data=sensor_data)
+
+@app.route('/led', methods=['POST'])
+def led():
+    state = request.form.get('state')
+    control_led(state)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
